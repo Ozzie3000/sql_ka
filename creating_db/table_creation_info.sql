f885d285c905bd9740f0ca6297824cc42545c450
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-14 23:04:36 -0400

    insert data into table

--- creating_db/table_creation_info.sql
+++ creating_db/table_creation_info.sql
@@ -11,9 +11,9 @@ Dark chocolate (3)
 CREATE TABLE Groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER);
 
 /** Insert the first row **/
-INSERT INTO Groceries VALuES (1, "Apples", 7);
-INSERT INTO Groceries VALuES (2, "Bananas", 7);
-INSERT INTO Groceries VALuES (3, "Dark chocolate", 3);
+INSERT INTO Groceries Values (1, "Apples", 7);
+INSERT INTO Groceries Values (2, "Bananas", 7);
+INSERT INTO Groceries Values (3, "Dark chocolate", 3);
 
 /** Select EVERYTHING from the groceries table **/
 SELECT * FROM Groceries;
