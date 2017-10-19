61f7d54e763a05e292dc3b62a2a58b4013e35b2f
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-19 08:44:49 -0400

    sql. insert dba. movies

--- basics/query_db/movies.sql
+++ basics/query_db/movies.sql
@@ -1,11 +1,13 @@
 /** Create db for movies **/
-CREATE TABLE movies (id INTEGER PRIMARY KEY, name TEXT, release_year INTEGER);
+CREATE TABLE Movies (id INTEGER PRIMARY KEY, name TEXT, release_year INTEGER);
 
-/** go wild and pick some movies to seed db **/
-INSERT INTO movies VALUES (1, "Avatar", 2009);
-INSERT INTO movies VALUES (2, "Jaws", 1975);
-INSERT INTO movies VALUES (3, "Star Wars: Episode IV - A New Hope", 1977);
-INSERT INTO movies VALUES (4, "Primer", 2004);
-INSERT INTO movies VALUES (5, "The Lion King", 1994);
-INSERT INTO movies VALUES (6, "The Babadook", 2014);
- 
+/** go wild and pick some Movies to seed db **/
+INSERT INTO Movies VALUES (1, "Avatar", 2009);
+INSERT INTO Movies VALUES (2, "Jaws", 1975);
+INSERT INTO Movies VALUES (3, "Star Wars: Episode IV - A New Hope", 1977);
+INSERT INTO Movies VALUES (4, "Primer", 2004);
+INSERT INTO Movies VALUES (5, "The Lion King", 1994);
+INSERT INTO Movies VALUES (6, "The Babadook", 2014);
+
+/** get all rows from Movies DB **/
+SELECT * FROM Movies;
