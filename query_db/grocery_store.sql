a3c514ced8812951e142e0ce2009e344232fbe86
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-18 20:35:31 -0400

    query db.

--- query_db/grocery_store.sql
+++ query_db/grocery_store.sql
@@ -1,3 +1,10 @@
 /** CREATE TABLE Groceries ();  **/
 /**Notice we  make a PK but it does not auto increment  **/
 CREATE TABLE Groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
+
+
+/** Insert the first row **/
+INSERT INTO Books Values (1, "Apples", 7);
+INSERT INTO Books Values (2, "Bananas", 7);
+INSERT INTO Books Values (3, "Dark chocolate", 3);
+
