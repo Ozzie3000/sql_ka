bcefaaafcc7a765dd58341850af1ccbc50ee1898
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-18 20:35:14 -0400

    query db.

---
+++ query_db/grocery_store.sql
@@ -0,0 +1,3 @@
+/** CREATE TABLE Groceries ();  **/
+/**Notice we  make a PK but it does not auto increment  **/
+CREATE TABLE Groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
