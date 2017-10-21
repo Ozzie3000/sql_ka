73760e8f98540dc322d22e4fe1ca734936273225
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-21 18:43:22 -0400

    sql. to do list.

---
+++ basics/query_db/todo_list.sql
@@ -0,0 +1,8 @@
+/** a to do list.  **/
+CREATE TABLE Todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
+INSERT INTO Todo_list VALUES (1, "Empty dishwasher", 5);
+INSERT INTO Todo_list VALUES (2, "vacuuming", 40);
+INSERT INTO Todo_list VALUES (3, "Learn some stuff on computer science", 60);
+
+/**pick something fun and insert it into table**/
+INSERT INTO Todo_list VALUES (4, "Look for Droids", 360); 
