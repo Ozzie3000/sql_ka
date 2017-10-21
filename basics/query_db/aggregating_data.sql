b87bca3eea380c3d27b4b2339cf270aada97087a
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-20 21:55:15 -0400

    sql. aggregating data

--- basics/query_db/aggregating_data.sql
+++ basics/query_db/aggregating_data.sql
@@ -10,3 +10,20 @@ INSERT INTO Groceries VALUES (3, "Dark chocolate", 3, 1);
 INSERT INTO Groceries VALUES (4, "Eggplant", 1, 2);
 INSERT INTO Groceries VALUES (5, "Eggs", 12, 12);
 INSERT INTO Groceries VALUES (6, "Figs", 6, 2);
+
+/** I like to know total so we know we got everything **/
+/** SELECT SUM(column_name) FROM Table_name; **/
+SELECT SUM(quantity) FROM Groceries;
+/**sum should be 36**/
+
+/** what is the most bought single item **/
+SELECT MAX(quantity) FROM Groceries;
+/** shows 12, but not what item**/
+
+/**make sure right items after each aisle **/
+SELECT SUM(quantity) FROM Groceries GROUP BY aisle;
+/**we only get quanities for each aisle. **/
+
+/**Add aisle to see sum for that aisle **/
+SELECT aisle, SUM(quantity) FROM Groceries GROUP BY aisle;
+/**now we see sum of items per aisle number**/
