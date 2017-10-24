ee00be9aa042f969f3009133ca9957a739cc2180
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-24 09:39:18 -0400

    sql. project. store db. insert into db

--- basics/store_db/store_db_design.sql
+++ basics/store_db/store_db_design.sql
@@ -3,4 +3,9 @@
 CREATE TABLE Books (id INTEGER, author TEXT, category TEXT, pages INTEGER, price FLOAT(3000,2));
 **/
 
-CREATE TABLE Books (id INTEGER, author TEXT, category TEXT, pages INTEGER, price INTEGER);
+CREATE TABLE Books (id INTEGER, author TEXT, title TEXT, category TEXT, pages INTEGER, price INTEGER);
+
+INSERT INTO Books VALUES (1, "John Steinbeck", "The Winter of our Discontent", "fiction", 311, 13);
+
+/**test to see first row**/
+SELECT * FROM Books;
