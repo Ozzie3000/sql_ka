ca3168ca2f10543b10901262c7ebf29b5d03d397
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-24 10:29:35 -0400

    sql. project. store db. insert into db

--- basics/store_db/store_db_design.sql
+++ basics/store_db/store_db_design.sql
@@ -6,6 +6,20 @@ CREATE TABLE Books (id INTEGER, author TEXT, category TEXT, pages INTEGER, price
 CREATE TABLE Books (id INTEGER, author TEXT, title TEXT, category TEXT, pages INTEGER, price INTEGER);
 
 INSERT INTO Books VALUES (1, "John Steinbeck", "The Winter of our Discontent", "fiction", 311, 13);
-
-/**test to see first row**/
+/**test to see first row
 SELECT * FROM Books;
+**/
+INSERT INTO Books VALUES (2, "Ernest Hemingway", "For Whom the Bell Tolls", "fiction", 480, 11);
+INSERT INTO Books VALUES (3, "", "", "", 1, 1);
+INSERT INTO Books VALUES (4, "", "", "", 1, 1);
+INSERT INTO Books VALUES (5, "", "", "", 1, 1);
+INSERT INTO Books VALUES (6, "", "", "", 1, 1);
+INSERT INTO Books VALUES (7, "", "", "", 1, 1);
+INSERT INTO Books VALUES (8, "", "", "", 1, 1);
+INSERT INTO Books VALUES (9, "", "", "", 1, 1);
+INSERT INTO Books VALUES (10, "", "", "", 1, 1);
+INSERT INTO Books VALUES (11, "", "", "", 1, 1);
+INSERT INTO Books VALUES (12, "", "", "", 1, 1);
+INSERT INTO Books VALUES (13, "", "", "", 1, 1);
+INSERT INTO Books VALUES (14, "", "", "", 1, 1);
+INSERT INTO Books VALUES (15, "", "", "", 1, 1);
