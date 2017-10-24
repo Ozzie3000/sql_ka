f93814c2f8ce6c42c5c4e4de590b095afe1cbec3
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-24 09:01:55 -0400

    sql. project. store db. create db

--- basics/store_db/store_db_design.sql
+++ basics/store_db/store_db_design.sql
@@ -1,2 +1,6 @@
 /** Store the goods or good are stored here **/
+/** the interactive tool doesn't like my float; gotta learn how do do this later. Using whole currency
 CREATE TABLE Books (id INTEGER, author TEXT, category TEXT, pages INTEGER, price FLOAT(3000,2));
+**/
+
+CREATE TABLE Books (id INTEGER, author TEXT, category TEXT, pages INTEGER, price INTEGER);
