0cee76cc519ba88b0adbbfb4e9d8f82e572eb10e
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-25 07:59:52 -0400

    sql. project. create db. insert books

--- basics/store_db/store_db_design.sql
+++ basics/store_db/store_db_design.sql
@@ -11,8 +11,8 @@ SELECT * FROM Books;
 **/
 INSERT INTO Books VALUES (2, "Ernest Hemingway", "For Whom the Bell Tolls", "fiction", 480, 11);
 INSERT INTO Books VALUES (3, "Don Quixote", "Miguel de Cervantes", "fiction", 114 , 12);
-INSERT INTO Books VALUES (4, "", "", "", 1, 1);
-INSERT INTO Books VALUES (5, "", "", "", 1, 1);
+INSERT INTO Books VALUES (4, "In Search of Lost Time", "Marcel Proust", "fiction", 606, 14);
+INSERT INTO Books VALUES (5, "Ulysses", "James Joyce", "fiction", 412, 9);
 INSERT INTO Books VALUES (6, "", "", "", 1, 1);
 INSERT INTO Books VALUES (7, "", "", "", 1, 1);
 INSERT INTO Books VALUES (8, "", "", "", 1, 1);
