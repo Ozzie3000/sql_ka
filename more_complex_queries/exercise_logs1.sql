287968112a3fe2040a4fc8ea39562d64376fbe97
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-26 19:36:15 -0400

    sql. more complex. exercies logs

--- more_complex_queries/exercise_logs1.sql
+++ more_complex_queries/exercise_logs1.sql
@@ -4,3 +4,9 @@ CREATE TABLE Excercise_logs
 		minutes INTEGER,
 		calories INTEGER,
 		heart_rate INTEGER);
+
+/** drop some pretend data because you know you don't do cool stuff like wrestle bears**/
+/**what do I do if I forget order? **/
+/**specify columns first**/
+/**then pass values**/
+INSERT INTO Exercise_logs (type, minutes, heart_rate, calories) VALUES ("rowing", 30, 135, 120);
