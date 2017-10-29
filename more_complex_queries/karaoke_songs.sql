0554d5b7b95911b4f64bcd9b610e8e36c98fab3a
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-29 13:36:53 -0400

    sql. more complex. karaoke song selector

--- more_complex_queries/karaoke_songs.sql
+++ more_complex_queries/karaoke_songs.sql
@@ -8,18 +8,18 @@ CREATE TABLE Songs (
 	releast INTEGER);
 
 INSERT INTO Songs (title, artist, mood, duration, released)
-    VALUES ("", "", "", , 19);
+    VALUES ("song a", "artist a", "mood a", 120, 1920);
 INSERT INTO Songs (title, artist, mood, duration, released)
-    VALUES ("", "", "", , 20);
+    VALUES ("song b", "artist b", "mood b", 130, 1930);
 INSERT INTO Songs (title, artist, mood, duration, released)
-    VALUES ("", "", "", , 19);
+    VALUES ("song c", "artist c", "mood c", 140, 1940);
 INSERT INTO Songs (title, artist, mood, duration, released)
-    VALUES ("", "", "", , 19);
+    VALUES ("song d", "artist d", "mood d", 150, 1950);
 INSERT INTO Songs (title, artist, mood, duration, released)
-    VALUES ("", "", "", , 19);
+    VALUES ("song e", "artist e", "mood e", 160, 1960);
 INSERT INTO Songs (title, artist, mood, duration, released)
-    VALUES ("", "", "", , 19);
+    VALUES ("song f", "artist f", "mood f", 170, 1970);
 INSERT INTO Songs (title, artist, mood, duration, released)
-    VALUES ("", "", "", , 19);
+    VALUES ("song g", "artist g", "mood g", 180, 1980);
 INSERT INTO Songs (title, artist, mood, duration, released)
-    VALUES ("", "", "", , 20);
+    VALUES ("song h", "artist h", " mood h", 190, 1990);
