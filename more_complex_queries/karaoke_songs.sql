bb41efd5322c6734fe8c4f279a95f9fd0cf734e8
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-28 20:59:06 -0400

    sql. more complex. karaoke song selector

--- more_complex_queries/karaoke_songs.sql
+++ more_complex_queries/karaoke_songs.sql
@@ -1,4 +1,25 @@
 /** **/
 CREATE TABLE Songs (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	)
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+	artist TEXT,
+	mood TEXT,
+	duration INTEGER,
+	releast INTEGER);
+
+INSERT INTO Songs (title, artist, mood, duration, released)
+    VALUES ("", "", "", , 19);
+INSERT INTO Songs (title, artist, mood, duration, released)
+    VALUES ("", "", "", , 20);
+INSERT INTO Songs (title, artist, mood, duration, released)
+    VALUES ("", "", "", , 19);
+INSERT INTO Songs (title, artist, mood, duration, released)
+    VALUES ("", "", "", , 19);
+INSERT INTO Songs (title, artist, mood, duration, released)
+    VALUES ("", "", "", , 19);
+INSERT INTO Songs (title, artist, mood, duration, released)
+    VALUES ("", "", "", , 19);
+INSERT INTO Songs (title, artist, mood, duration, released)
+    VALUES ("", "", "", , 19);
+INSERT INTO Songs (title, artist, mood, duration, released)
+    VALUES ("", "", "", , 20);
