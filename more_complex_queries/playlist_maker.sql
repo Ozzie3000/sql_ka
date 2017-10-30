11c3ec42d9926a734afe647783125678e690865c
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-30 19:26:46 -0400

    sql. playlist maker

---
+++ more_complex_queries/playlist_maker.sql
@@ -0,0 +1,60 @@
+CREATE TABLE Artists (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    country TEXT,
+    genre TEXT);
+
+INSERT INTO Artists (name, country, genre)
+    VALUES ("Elvis Presley", "US", "Rock");
+INSERT INTO Artists (name, country, genre)
+    VALUES ("Led Zeppelin", "US", "Hard rock");
+INSERT INTO Artists (name, country, genre)
+    VALUES ("ABBA", "Sweden", "Disco");
+INSERT INTO Artists (name, country, genre)
+    VALUES ("Oasis", "UK", "Britpop");
+INSERT INTO Artists (name, country, genre)
+    VALUES ("Rush", "Canada", "Progressive metal");
+INSERT INTO Artists (name, country, genre)
+    VALUES ("Meatloaf", "US", "Hard rock");
+INSERT INTO Artists (name, country, genre)
+    VALUES ("Bert Weedon", "UK", "Country");
+INSERT INTO Artists (name, country, genre)
+    VALUES ("Shania Twain", "Canada", "Country");
+INSERT INTO Artists (name, country, genre)
+    VALUES ("Maroon 5", "US", "Pop");
+INSERT INTO Artists (name, country, genre)
+    VALUES ("Guns N' Roses", "US", "Hard rock");
+INSERT INTO Artists (name, country, genre)
+    VALUES ("Patsy Cline", "US", "Country");
+INSERT INTO Artists (name, country, genre)
+    VALUES ("Bob Marley", "Jamaica", "Reggae");
+
+CREATE TABLE Songs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    artist TEXT,
+    title TEXT);
+
+INSERT INTO Songs (artist, title)
+    VALUES ("Elvis Presley", "Return to Sender");
+INSERT INTO Songs (artist, title)
+    VALUES ("Maroon 5", "Sugar");
+INSERT INTO Songs (artist, title)
+    VALUES ("Rush", "YYZ");
+INSERT INTO Songs (artist, title)
+    VALUES ("Rush", "Closer to the Heart");
+INSERT INTO Songs (artist, title)
+    VALUES ("Bert Weedon", "China Doll");
+INSERT INTO Songs (artist, title)
+    VALUES ("Patsy Cline", "I Fall to Pieces");
+INSERT INTO Songs (artist, title)
+    VALUES ("Led Zeppelin", "Stairway to heaven");
+INSERT INTO Songs (artist, title)
+    VALUES ("ABBA", "Mamma mia");
+INSERT INTO Songs (artist, title)
+    VALUES ("Oasis", "Wonderwall");
+INSERT INTO Songs (artist, title)
+    VALUES ("Oasis", "Champagne Supernova");
+INSERT INTO Songs (artist, title)
+    VALUES ("Guns N' Roses", "Welcome to the Jungle");
+    
+
