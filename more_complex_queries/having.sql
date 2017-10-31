42a07d1c6656ae72eceea6a36a7cd06060de5c56
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-31 10:46:56 -0400

    sql. having

---
+++ more_complex_queries/having.sql
@@ -0,0 +1,16 @@
+/**exercise logs 3**/
+CREATE TABLE Exercise_logs
+	(id INTEGER PRIMARY KEY AUTOINCREMENT,
+		type TEXT,
+		minutes INTEGER,
+		calories INTEGER,
+		heart_rate INTEGER);
+
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("biking", 10, 35, 110);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("biking", 30, 100, 105);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("mountain climbers", 10, 150, 170);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("mountain climbers", 5, 75, 165);
+INSERT INTO Exercise_logs (type, minutes, heart_rate, calories) VALUES ("rowing", 30, 135, 120);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("swimming", 20, 100, 145);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("treadmill", 60, 600, 150);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("treadmill", 30, 300, 150);
