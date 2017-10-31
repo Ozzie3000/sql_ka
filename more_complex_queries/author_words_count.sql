65b840c28eac92c61c46eac36bdab243ca98f4e3
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-31 12:55:38 -0400

    sql. book and words. having group by

---
+++ more_complex_queries/author_words_count.sql
@@ -0,0 +1,33 @@
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author TEXT,
+    title TEXT,
+    words INTEGER);
+    
+INSERT INTO books (author, title, words)
+    VALUES ("Peter F Hamilton", "The Reality Dysfunction", 385000);
+INSERT INTO books (author, title, words)
+    VALUES ("Peter F Hamilton", "THe Neutronium Alchemist", 393000);
+INSERT INTO books (author, title, words)
+    VALUES ("Peter F Hamilton", "The Naked God", 469000);
+
+
+INSERT INTO books (author, title, words)
+    VALUES ("George R R Martin", "A Game of Thrones", 298000);
+INSERT INTO books (author, title, words)
+    VALUES ("George R R Martin", "A Clash of Kings ", 326000);
+INSERT INTO books (author, title, words)
+    VALUES ("George R R Martin", "A Storm of Swords", 424000);
+INSERT INTO books (author, title, words)
+    VALUES ("George R R Martin", "A Feast of Crows", 300000);
+INSERT INTO books (author, title, words)
+    VALUES ("George R R Martin", "A Dance with Dragons", 422000);
+    
+INSERT INTO books (author, title, words)
+    VALUES ("J.R.R. Tolkien", "The Hobbit", 95022);
+INSERT INTO books (author, title, words)
+    VALUES ("J.R.R. Tolkien", "Fellowship of the Ring", 177227);
+INSERT INTO books (author, title, words)
+    VALUES ("J.R.R. Tolkien", "Two Towers", 143436);
+INSERT INTO books (author, title, words)
+    VALUES ("J.R.R. Tolkien", "Return of the King", 134462);
