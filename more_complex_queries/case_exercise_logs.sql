7b50a48ef71427ce01dec280baf6f12cff2a0441
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-01 18:16:43 -0400

    sql. case. exercise logs 4

---
+++ more_complex_queries/case_exercise_logs.sql
@@ -0,0 +1,21 @@
+/**exercise logs 4**/
+CREATE TABLE Exercise_logs
+	(id INTEGER PRIMARY KEY AUTOINCREMENT,
+		type TEXT,
+		minutes INTEGER,
+		calories INTEGER,
+		heart_rate INTEGER);
+
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("biking", 10, 35, 110);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("biking", 20, 75, 110);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("biking", 30, 100, 105);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("mountain climbers", 10, 150, 170);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("mountain climbers", 5, 75, 165);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("mountain climbers", 6, 76, 165);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("rowing", 15, 67, 120);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("rowing", 30, 135, 120);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("swimming", 20, 100, 145);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("swimming", 10, 50, 145);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("treadmill", 60, 600, 150);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("treadmill", 30, 300, 150);
+INSERT INTO Exercise_logs (type, minutes, calories, heart_rate) VALUES ("treadmill", 45, 150, 150);
