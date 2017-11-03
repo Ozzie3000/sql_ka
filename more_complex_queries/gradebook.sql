fa138efa335a849e6b8460d9c33f0c52ca7b60ab
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-02 21:12:02 -0400

    sql. grade book progject

--- more_complex_queries/gradebook.sql
+++ more_complex_queries/gradebook.sql
@@ -20,3 +20,18 @@ INSERT INTO student_grades (name, number_grade, fraction_completed)
 
 /**  select all of the rows, and display the name, number_grade, and percent_completed, which you can compute by multiplying and rounding the fraction_completed column.  (by 100 to get percent)**/    
 SELECT name, number_grade, ROUND(fraction_completed * 100) AS percent_completed FROM student_grades;
+
+/** a table that shows how many students have earned which letter_grade. You can output the letter_grade 
+by using CASE with the number_grade column, outputting 'A' for grades > 90, 'B' for grades > 80, 
+'C' for grades > 70, and 'F' otherwise. Then you can use COUNT with GROUP BY to show the number of 
+students with each of those grades**/
+
+SELECT number_grade,
+    CASE
+        WHEN number_grade > 90 THEN "A"
+        WHEN number_grade > 80 THEN "B"
+        WHEN number_grade > 70 THEN "C"
+        ELSE "F"
+    END as "letter_grade"
+FROM student_grades
+GROUP BY letter_grade;
