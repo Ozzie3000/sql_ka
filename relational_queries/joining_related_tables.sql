bce1e38a05633cf10a8f5c1d824c0c35b79b69bd
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-04 20:39:13 -0400

    sql. relational queries. joining related tables

--- relational_queries/joining_related_tables.sql
+++ relational_queries/joining_related_tables.sql
@@ -1,3 +1,4 @@
+/*table for peeps*/
 CREATE TABLE students (id INTEGER PRIMARY KEY,
     first_name TEXT,
     last_name TEXT,
@@ -7,3 +8,9 @@ CREATE TABLE students (id INTEGER PRIMARY KEY,
 
 INSERT INTO students (first_name, last_name, email, phone, birthdate) VALUES ("Peter", "Rabbit", "peter@rabbit.com", "555-5555", "2000-01-01"); 
 INSERT INTO students (first_name, last_name, email, phone, birthdate) VALUES ("Alice", "Wonderland", "alice@wonderland.com", "555-6666", "2000-01-02");
+
+/*table for grades*/
+CREATE TABLE student_grades (id INTEGER PRIMARY KEY,
+    student_id INTEGER,
+    test TEXT,
+    grade INTEGER);
