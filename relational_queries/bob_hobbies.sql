5b67ad9d9186ae3e138ac221ad15b2ef68995ad6
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-05 15:32:01 -0500

    sql. relational queries. bob hobbies challenge

---
+++ relational_queries/bob_hobbies.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Persons (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER);
+    
+INSERT INTO persons (name, age) VALUES ("Hugo Richard", 23);
+INSERT INTO persons (name, age) VALUES ("Judy Garland", 25);
+INSERT INTO persons (name, age) VALUES ("Fred  Astaire", 44);
+INSERT INTO persons (name, age) VALUES ("Ginger Rogers", 20);
+INSERT INTO persons (name, age) VALUES ("Johnny JoJackson", 38);
