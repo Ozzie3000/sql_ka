20d006b1b0a0760e52ce1e41c68fe25c6326c21e
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-05 15:45:07 -0500

    sql. relational queries. bob hobbies challenge

--- relational_queries/bob_hobbies.sql
+++ relational_queries/bob_hobbies.sql
@@ -3,12 +3,12 @@ CREATE TABLE Persons (
     name TEXT,
     age INTEGER);
     
-INSERT INTO persons (name, age) VALUES ("Hugo Richard", 23);
-INSERT INTO persons (name, age) VALUES ("Judy Garland", 25);
-INSERT INTO persons (name, age) VALUES ("Fred  Astaire", 44);
-INSERT INTO persons (name, age) VALUES ("Ginger Rogers", 20);
-INSERT INTO persons (name, age) VALUES ("Johnny JoJackson", 38);
-INSERT INTO persons (name, age) VALUES ("Bob UpnDown", 23);
+INSERT INTO Persons (name, age) VALUES ("Hugo Richard", 23);
+INSERT INTO Persons (name, age) VALUES ("Judy Garland", 25);
+INSERT INTO Persons (name, age) VALUES ("Fred  Astaire", 44);
+INSERT INTO Persons (name, age) VALUES ("Ginger Rogers", 20);
+INSERT INTO Persons (name, age) VALUES ("Johnny JoJackson", 38);
+INSERT INTO Persons (name, age) VALUES ("Bob UpnDown", 23);
 
 
 CREATE table Hobbies (
@@ -16,13 +16,20 @@ CREATE table Hobbies (
     person_id INTEGER,
     name TEXT);
     
-INSERT INTO hobbies (person_id, name) VALUES (1, "drawing");
-INSERT INTO hobbies (person_id, name) VALUES (1, "coding");
-INSERT INTO hobbies (person_id, name) VALUES (2, "dancing");
-INSERT INTO hobbies (person_id, name) VALUES (2, "singing");
-INSERT INTO hobbies (person_id, name) VALUES (3, "dancing");
-INSERT INTO hobbies (person_id, name) VALUES (3, "singing");
-INSERT INTO hobbies (person_id, name) VALUES (3, "acting");
-INSERT INTO hobbies (person_id, name) VALUES (4, "dancing");
-INSERT INTO hobbies (person_id, name) VALUES (5, "hidding");
-INSERT INTO hobbies (person_id, name) VALUES (6, "not sinking");
+INSERT INTO Hobbies (person_id, name) VALUES (1, "drawing");
+INSERT INTO Hobbies (person_id, name) VALUES (1, "coding");
+INSERT INTO Hobbies (person_id, name) VALUES (2, "dancing");
+INSERT INTO Hobbies (person_id, name) VALUES (2, "singing");
+INSERT INTO Hobbies (person_id, name) VALUES (3, "dancing");
+INSERT INTO Hobbies (person_id, name) VALUES (3, "singing");
+INSERT INTO Hobbies (person_id, name) VALUES (3, "acting");
+INSERT INTO Hobbies (person_id, name) VALUES (4, "dancing");
+INSERT INTO Hobbies (person_id, name) VALUES (5, "hidding");
+INSERT INTO Hobbies (person_id, name) VALUES (6, "not sinking");
+
+
+/*insert a person*/
+INSERT INTO Persons (name, age) VALUES ("John Fogerty");
+
+/*insert a hobby*/
+INSERT INTO Hobbies (person_id, name) VALUES (7, "Rocking out");
