42874f318b5adbae39d00a6b72d30a205a6aafbc
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-05 20:48:49 -0500

    sql. relational queries. left outer joins

--- relational_queries/left_outer_joins.sql
+++ relational_queries/left_outer_joins.sql
@@ -8,6 +8,7 @@ CREATE TABLE Students (id1 INTEGER PRIMARY KEY,
 
 INSERT INTO Students (first_name, last_name, email, phone, birthdate) VALUES ("Peter", "Rabbit", "peter@rabbit.com", "555-5555", "2000-01-01"); 
 INSERT INTO Students (first_name, last_name, email, phone, birthdate) VALUES ("Alice", "Wonderland", "alice@wonderland.com", "555-6666", "2000-01-02");
+INSERT INTO Students (first_name, last_name, email, phone, birthdate) VALUES ("Richard", "Anderson", "richarddeananderson@sg.com", "555-7777", "1960-01-03");
 
 /*table for grades*/
 CREATE TABLE Student_grades (id2 INTEGER PRIMARY KEY,
@@ -36,3 +37,4 @@ INSERT INTO Student_projects (student_id, title) VALUES (2, "Always teleport hom
 SELECT Students.first_name, Students.last_name, Student_projects.title FROM Students
     JOIN Student_projects
     ON Students.id1 = Student_projects.student_id;
+/*notice Richard is missing*/
