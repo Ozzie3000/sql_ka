3abd786e0a7ce412fd8296a638079f0335da8072
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-07 16:26:17 -0500

    sql. relational queries. self joins

--- relational_queries/self_joins.sql
+++ relational_queries/self_joins.sql
@@ -14,3 +14,19 @@ INSERT INTO Students
     VALUES (3, "Naruto", "Uzumaki", "nuzumaki@hiddenleaf.com", "555-3333", "2001-05-10", 4);
 INSERT INTO Students 
     VALUES (4, "Sasuke", "Uchiha", "suchiha@hiddenleaf.com", "555-4444", "2001-12-24", 3);
+
+/*normal select*/
+SELECT Students.id, Students.first_name, Students.last_name, Students.buddy_id FROM Students;
+
+/*name of each student and show buddies email from same table*/
+SELECT Students.id, Students.first_name, Students.last_name, Students.buddy_id 
+    FROM Students
+    JOIN Students;
+/*2 problems the select Students. could be either one; ambigous
+and we should rename as alias to distinguish*/
+
+/*also need an ON to match foreign key from students to Primary key on buddies table*/
+SELECT Students.id, Students.first_name, Students.last_name, Buddies.email 
+    FROM Students
+    JOIN Students Buddies
+    ON Students.buddy_id = Buddies.id;
