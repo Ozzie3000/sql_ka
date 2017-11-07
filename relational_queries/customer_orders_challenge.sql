6e7662e5519d0952b1a2d2df2488ec039aa3d028
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-07 14:43:52 -0500

    sql. relational queries. customer orders challenge

--- relational_queries/customer_orders_challenge.sql
+++ relational_queries/customer_orders_challenge.sql
@@ -1,21 +1,35 @@
-CREATE TABLE customers (
+CREATE TABLE Customers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     email TEXT);
     
-INSERT INTO customers (name, email) VALUES ("Doctor Who", "doctorwho@timelords.com");
-INSERT INTO customers (name, email) VALUES ("Harry Potter", "harry@potter.com");
-INSERT INTO customers (name, email) VALUES ("Captain Awesome", "captain@awesome.com");
+INSERT INTO Customers (name, email) VALUES ("Doctor Who", "doctorwho@timelords.com");
+INSERT INTO Customers (name, email) VALUES ("Harry Potter", "harry@potter.com");
+INSERT INTO Customers (name, email) VALUES ("Captain Awesome", "captain@awesome.com");
 
-CREATE TABLE orders (
+CREATE TABLE Orders (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     customer_id INTEGER,
     item TEXT,
     price REAL);
 
-INSERT INTO orders (customer_id, item, price)
+INSERT INTO Orders (customer_id, item, price)
     VALUES (1, "Sonic Screwdriver", 3000.00);
-INSERT INTO orders (customer_id, item, price)
+INSERT INTO Orders (customer_id, item, price)
     VALUES (2, "Titanium frame eye glasses", 350.00);
-INSERT INTO orders (customer_id, item, price)
+INSERT INTO Orders (customer_id, item, price)
     VALUES (1, "Trench coat", 200.00);
+
+/*shows all customers even ones with no order. SHow name, email, item, price of orders*/
+SELECT Customers.name, Customers.email, Orders.item, Orders.price FROM Customers
+    LEFT OUTER JOIN Orders
+    ON Customers.id = Orders.customer_id;
+
+/*create another query that will result in one row per each customer, 
+with their name, email, and total amount of money they've spent on orders. 
+Sort the rows according to the total money spent, from the most spent to the least spent
+*/
+SELECT Customers.name, Customers.email, SUM(price) AS totalspent FROM Customers
+    LEFT OUTER JOIN Orders
+    ON Customers.id = Orders.customer_id
+    GROUP BY totalspent DESC
