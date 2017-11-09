258d5b3a90b71b8646f245a9d74fc09056d78161
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-08 19:25:18 -0500

    sql. relational queries. famous people project

--- relational_queries/famous_peple.sql
+++ relational_queries/famous_peple.sql
@@ -15,22 +15,32 @@ INSERT INTO Famous_peeps (firstname, lastname) VALUES ("james", "stewart");
 INSERT INTO Famous_peeps (firstname, lastname) VALUES ("gloria", "mclean");
 INSERT INTO Famous_peeps (firstname, lastname) VALUES ("humphrey", "bogart");
 INSERT INTO Famous_peeps (firstname, lastname) VALUES ("helen", "menken");
-INSERT INTO Famous_peeps (firstname, lastname) VALUES ("camktry", "grant");
+INSERT INTO Famous_peeps (firstname, lastname) VALUES ("cary", "grant");
 
 CREATE TABLE Movies (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	Famous_peeps_id INTEGER,
+	famous_peeps_id INTEGER,
 	movie_name TEXT);
 
-INSERT INTO Movies (movie_name) VALUES (1, "rebel without a cause");
-INSERT INTO Movies (movie_name) VALUES (2, "jet pilot");
-INSERT INTO Movies (movie_name) VALUES (3, "the alamo");
-INSERT INTO Movies (movie_name) VALUES (4, "the wizard of oz");
-INSERT INTO Movies (movie_name) VALUES (6, "shop worm angel");
-INSERT INTO Movies (movie_name) VALUES (8, "the maltese falcon");
-INSERT INTO Movies (movie_name) VALUES (10, "none but the lonely heart");
+INSERT INTO Movies (famous_peeps_id, movie_name) VALUES (1, "rebel without a cause");
+INSERT INTO Movies (famous_peeps_id, movie_name) VALUES (2, "jet pilot");
+INSERT INTO Movies (famous_peeps_id, movie_name) VALUES (3, "the alamo");
+INSERT INTO Movies (famous_peeps_id, movie_name) VALUES (4, "the wizard of oz");
+INSERT INTO Movies (famous_peeps_id, movie_name) VALUES (6, "shop worm angel");
+INSERT INTO Movies (famous_peeps_id, movie_name) VALUES (8, "the maltese falcon");
+INSERT INTO Movies (famous_peeps_id, movie_name) VALUES (10, "none but the lonely heart");
 
 CREATE TABLE Married (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	personid1 INTEGER,
 	personid2 INTEGER);
+
+/*Movie stars: What movies are they in?*/
+SELECT * FROM Famous_peeps
+	JOIN Movies
+	ON Famous_peeps.id = Movies.Famous_peeps_id;
+
+/* just person and movie columns*/*/
+SELECT Famous_peeps.firstname, Famous_peeps.lastname, Movies.movie_name FROM Famous_peeps
+	JOIN Movies
+	ON Famous_peeps.id = Movies.Famous_peeps_id;
