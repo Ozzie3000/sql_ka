6d9a312f2645bc0132d2202e18eae2d468f8e4ea
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-09 21:00:56 -0500

    sql. modifying dbs. update delete

--- modifying_db_w_sql/update_delete.sql
+++ modifying_db_w_sql/update_delete.sql
@@ -11,14 +11,20 @@ CREATE TABLE Diary_logs (
     );
 
 /* After user submitted their new diary log */
-INSERT INTO diary_logs (user_id, date, content) VALUES (1, "2013-12-11",
+INSERT INTO Diary_logs (user_id, date, content) VALUES (1, "2013-12-11",
     "I meet a guy named OhNoesGuy.  He loves to eat his weight in chocolate.");
     
-INSERT INTO diary_logs (user_id, date, content) VALUES (1, "2013-12-11",
+INSERT INTO Diary_logs (user_id, date, content) VALUES (1, "2013-12-11",
     "OhNoesGuy taught me how to make a mean sundae with lots of chocolate.");
 
-INSERT INTO diary_logs (user_id, date, content) VALUES (1, "2013-12-12",
+INSERT INTO Diary_logs (user_id, date, content) VALUES (1, "2013-12-12",
     "OhNoesGuy and I went out for some drinks with the crew.  Then we all went to watch a Warriors game.");
 
 /*check out logs so far*/    
 SELECT * FROM Diary_logs; 
+
+/*maybe you forgot some details and need to append to a log*/
+/*how do you pick a row? maybe you know values in some columns
+UPDATE Diary_logs SET content = "OhNoesGuy taught me how to make a mean sundae with lots of chocolate. His girlfriend was kind enough to let ups borrow the supplies"
+	WHERE user_id = 1 AND date = "2013-12-11"/
+*/
