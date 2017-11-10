67b1d00854dac218a36345471e451a2887f04917
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-09 20:54:18 -0500

    sql. modifying dbs. update delete

--- modifying_db_w_sql/update_delete.sql
+++ modifying_db_w_sql/update_delete.sql
@@ -1,3 +1,4 @@
+/*diary app*/
 CREATE TABLE Users (
     id INTEGER PRIMARY KEY,
     name TEXT);
@@ -8,3 +9,13 @@ CREATE TABLE Diary_logs (
     date TEXT,
     content TEXT
     );
+
+/* After user submitted their new diary log */
+INSERT INTO diary_logs (user_id, date, content) VALUES (1, "2013-12-11",
+    "I meet a guy named OhNoesGuy.  He loves to eat his weight in chocolate.");
+    
+INSERT INTO diary_logs (user_id, date, content) VALUES (1, "2013-12-11",
+    "OhNoesGuy taught me how to make a mean sundae with lots of chocolate.");
+
+INSERT INTO diary_logs (user_id, date, content) VALUES (1, "2013-12-12",
+    "OhNoesGuy and I went out for some drinks with the crew.  Then we all went to watch a Warriors game.");
