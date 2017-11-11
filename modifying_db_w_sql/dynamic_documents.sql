1b6a348524a926de2c2ec9580ed15d67fc064e6b
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-10 21:02:20 -0500

    sql. modifying dbs. dynamic documents

--- modifying_db_w_sql/dynamic_documents.sql
+++ modifying_db_w_sql/dynamic_documents.sql
@@ -16,3 +16,9 @@ INSERT INTO Documents (author, title, content)
     VALUES ("Jack the Lipper", "Things I'm Afraid Of", "Cosmological constant problem, Quantum Zeno effect, Infinite loops.");
 
 SELECT * FROM Documents;
+
+/*use UPDATE to change the author to 'Jack the Kipper' for all rows where it's currently 'Jack the Lipper'*/
+UPDATE Documents SET author = "Jack the Kipper" WHERE author = "Jack the Lipper";
+
+/*check to see if it worked */
+SELECT * FROM Documents;
