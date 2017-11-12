00810f7792c5954b060c89134c1128332ce4ca38
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-12 15:05:41 -0500

    sql. modifying dbs. altering table. clothes

---
+++ modifying_db_w_sql/clothing_alterations.sql
@@ -0,0 +1,11 @@
+CREATE TABLE clothes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    design TEXT);
+    
+INSERT INTO clothes (type, design)
+    VALUES ("coat", "black surcoat ");
+INSERT INTO clothes (type, design)
+    VALUES ("pants", "dark grey dress pants");
+INSERT INTO clothes (type, design)
+    VALUES ("tshirt", "charcoal triblend");
