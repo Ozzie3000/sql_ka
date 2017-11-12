4ea89db0ca5c6963dcadd58e4bc76f4e98440412
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-11 21:22:29 -0500

    sql. modifying dbs. altering tables after creation

--- modifying_db_w_sql/altering_tables_afterwards.sql
+++ modifying_db_w_sql/altering_tables_afterwards.sql
@@ -7,8 +7,7 @@ CREATE TABLE diary_logs (
     id INTEGER PRIMARY KEY,
     user_id INTEGER,
     date TEXT,
-    content TEXT,
-    emotion TEXT
+    content TEXT
     );
     
 /* After user submits a diary log */
@@ -19,6 +18,11 @@ INSERT INTO diary_logs (user_id, date, content) VALUES (1, "2010-11-12",
 So what do we do if we want to update the rows?  Alter
 ALTER TABLE tablename ADD columnname TYPE;
 */
+ALTER TABLE diary_logs ADD emotion TEXT;
+
 
 INSERT INTO diary_logs (user_id, date, content, emotion) VALUES (1, "2011-12-13", "I got to see an Orca today", "Excited");
 SELECT * FROM diary_logs;
+
+/*set default values you could have ran it like this instead*/
+/* ALTER TABLE diary_logs ADD emotion TEXT default "unknown";
