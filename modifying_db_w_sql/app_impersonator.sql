7e158a6285b31260aa6ce21f3f4e7ec7ecc0fd2e
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-12 21:24:45 -0500

    sql. modifying dbs. project. app impersonator

--- modifying_db_w_sql/app_impersonator.sql
+++ modifying_db_w_sql/app_impersonator.sql
@@ -10,8 +10,11 @@ CREATE TABLE tekken_scores(
 INSERT INTO tekken_scores (username, score, date) VALUES ("MADMAX", 751300, "1984-10-27");
 INSERT INTO tekken_scores (username, score, date) VALUES ("DUSTIN", 650990, "1984-06-12");
 INSERT INTO tekken_scores (username, score, date) VALUES ("LUCAS", 641183, "1983-12-29");
-INSERT INTO tekken_scores (username, score, date) VALUES ("MADMAX", 620784, "1984-02-17");
-INSERT INTO tekken_scores (username, score, date) VALUES ("MADMAX", 552415, "1984-09-07");
+INSERT INTO tekken_scores (username, score, date) VALUES ("WIZARD", 620784, "1984-02-17");
+INSERT INTO tekken_scores (username, score, date) VALUES ("HBIATC", 552415, "1984-09-07");
+
+/*UPDATE to emulate what happens when you edit data in the app.*/
+UPDATE tekken_scores SET username = "PRESTON", score = 555942, date = "1984-09-27" WHERE id = 5;
 
 
 /*checker*/
