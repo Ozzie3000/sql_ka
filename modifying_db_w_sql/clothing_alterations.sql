5a93e7573484ed691c62f36773f8e99a69297273
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-12 19:43:48 -0500

    sql. modifying dbs. altering table. clothes

--- modifying_db_w_sql/clothing_alterations.sql
+++ modifying_db_w_sql/clothing_alterations.sql
@@ -9,3 +9,9 @@ INSERT INTO clothes (type, design)
     VALUES ("pants", "dark grey dress pants");
 INSERT INTO clothes (type, design)
     VALUES ("tshirt", "charcoal triblend");
+
+/*Use ALTER to add a 'price' column to the table. */
+ALTER TABLE clothes ADD price INTEGER;
+
+/*select all the columns in each row to see what your table looks like now.*/
+SELECT * FROM clothes;
