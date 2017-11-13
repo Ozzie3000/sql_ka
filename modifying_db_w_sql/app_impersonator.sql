557c06507766f90e549e52996581d49fe9d0f75d
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-12 21:20:01 -0500

    sql. modifying dbs. project. app impersonator

--- modifying_db_w_sql/app_impersonator.sql
+++ modifying_db_w_sql/app_impersonator.sql
@@ -7,7 +7,11 @@ CREATE TABLE tekken_scores(
     date TEXT);
 
 /*insert data*/
-INSERT INTO tekken_scores (username, score, date) VALUES (MADMAX, 751300, "1984-10-27");
+INSERT INTO tekken_scores (username, score, date) VALUES ("MADMAX", 751300, "1984-10-27");
+INSERT INTO tekken_scores (username, score, date) VALUES ("DUSTIN", 650990, "1984-06-12");
+INSERT INTO tekken_scores (username, score, date) VALUES ("LUCAS", 641183, "1983-12-29");
+INSERT INTO tekken_scores (username, score, date) VALUES ("MADMAX", 620784, "1984-02-17");
+INSERT INTO tekken_scores (username, score, date) VALUES ("MADMAX", 552415, "1984-09-07");
 
 
 /*checker*/
