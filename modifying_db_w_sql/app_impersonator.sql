5b2fd7c643515e1088f14641d368542575a0a64e
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-12 21:15:24 -0500

    sql. modifying dbs. project. app impersonator

--- modifying_db_w_sql/app_impersonator.sql
+++ modifying_db_w_sql/app_impersonator.sql
@@ -1,4 +1,14 @@
 /* What does the app's SQL look like? */
 /*create table*/
 CREATE TABLE tekken_scores(
-    id INT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    score INTEGER,
+    date TEXT);
+
+/*insert data*/
+INSERT INTO tekken_scores (username, score, date) VALUES (MADMAX, 751300, "1984-10-27");
+
+
+/*checker*/
+SELECT * FROM tekken_scores;
