8e145520a60855f74c915054f7bdbfd73a91ac0d
Ozzie3000 <ozzieboy19@hotmail.com>
2017-11-12 19:49:14 -0500

    sql. modifying dbs. altering table. clothes

--- modifying_db_w_sql/clothing_alterations.sql
+++ modifying_db_w_sql/clothing_alterations.sql
@@ -15,3 +15,16 @@ ALTER TABLE clothes ADD price INTEGER;
 
 /*select all the columns in each row to see what your table looks like now.*/
 SELECT * FROM clothes;
+
+/*assign each item a price, using UPDATE - 
+item 1 should be 10 dollars, item 2 should be 20 dollars, item 3 should be 30 dollars.
+check all rows after*/
+UPDATE clothes SET price = 10 WHERE id = 1;
+UPDATE clothes SET price = 20 WHERE id = 2;
+UPDATE clothes SET price = 30 WHERE id = 3;
+SELECT * FROM clothes;
+
+/* insert a new item into the table that has all three attributes filled in, including 'price'. 
+Do one final SELECT of all the rows to check it worked*/
+INSERT INTO clothes (type, design, price) VALUES ("socks", "grey toe socks", 8);
+SELECT * FROM clothes;
